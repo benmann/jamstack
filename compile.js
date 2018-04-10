3486addf7ee08642a08a40be21b45e73dedecbae
Ben <ben@deteringdesign.de>
2018-04-10 14:57:32 +0200

    add config & watcher

--- compile.js
+++ compile.js
@@ -1,33 +1,55 @@
-const fs = require( 'fs' )
-const path = require( 'path' )
+const fs = require("fs")
+const path = require("path")
 
-const templateDirectory = "./"
-const partialDirectory = "./_partials/"
+const config = require("./jam.json")[0]
 
-fs.readdir(templateDirectory, function(err, files) {
-    files.forEach( function(file, index) {
-        if (path.extname(file) == ".html") {
+const templateDirectory = config.templates
+const partialDirectory = config.partials
+const dest = config.dest
 
-            fs.readFile(templateDirectory + file, 'utf8', function (err, content) {
-                let matches = content.match(/{{(.*?)}}/g)
-                let rendered
+if (!fs.existsSync(dest)) {fs.mkdirSync(dest)}
+if (!fs.existsSync(partialDirectory)) {fs.mkdirSync(partialDirectory)}
+if (!fs.existsSync(templateDirectory)) {fs.mkdirSync(templateDirectory)}
 
-                matches.forEach(function(match) {
-                    let partial = match.replace("{{", "").replace("}}", "").trim()
-                    let replacement = fs.readFileSync(partialDirectory + partial + ".html", "utf-8")
 
-                    content = content.replace(match, replacement)
-                })
-
-                rendered = content
-
-                let output = rendered.replace(/\r?\n|\r/g, "")
-                fs.writeFile("../" + file, output, function() {
-                    console.log(`Successfully compiled ${file}`)
-                })
-            })
-        }
-    }, function() {
-        process.exit()
-    })
+console.log(`Watching ${templateDirectory}.`)
+fs.watch(templateDirectory, {recursive: true, encoding: 'buffer'}, (eventType, filename) => {
+    compile()
 })
+
+
+function compile() {
+    console.log("➡  Compiling...")
+    fs.readdir(templateDirectory, function(err, files) {
+
+        if (!files) {
+            console.log(`⚠️  No files in template directory (${templateDirectory})... aborting.`)
+            process.exit()
+        }
+
+        files.forEach( function(file, index) {
+            if (path.extname(file) == ".html") {
+                fs.readFile(`${templateDirectory}/${file}`, "utf8", function (err, content) {
+                    let matches = content.match(/{{(.*?)}}/g)
+                    let rendered
+
+                    matches.forEach(function(match) {
+                        let partial = match.replace("{{", "").replace("}}", "").trim()
+                        let replacement = fs.readFileSync(`${partialDirectory}/${partial}.html`, "utf-8")
+
+                        content = content.replace(match, replacement)
+                    })
+
+                    rendered = content
+
+                    let output = rendered.replace(/\r?\n|\r/g, "")
+                    fs.writeFile(`${dest}/${file}`, output, function() {
+                        console.log(`✓  Successfully compiled ${file}`)
+                    })
+                })
+            }
+        }, function() {
+            process.exit()
+        })
+    })
+}
